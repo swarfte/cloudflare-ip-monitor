e1f1ea1045b0436d9d2c08c0d79613713a6d2cec
Benjamin Chau <68836494+swarfte@users.noreply.github.com>
2025-02-19 00:07:55 +0800

    setup the electron environment

---
+++ nuxt.config.ts
@@ -0,0 +1,33 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+
+export default defineNuxtConfig({
+  compatibilityDate: "2024-11-01",
+  devtools: { enabled: true },
+  devServer: {
+    port: Number(process.env.PORT as string),
+  },
+
+  eslint: {
+    // options here
+  },
+  electron: {
+    disableDefaultOptions: true,
+    build: [
+      {
+        // Main-Process entry file of the Electron App.
+        entry: "electron/main.ts",
+      },
+    ],
+  },
+  modules: [
+    "@nuxt/eslint",
+    "@nuxt/ui",
+    "@nuxt/fonts",
+    "@pinia/nuxt",
+    "pinia-plugin-persistedstate/nuxt",
+    "@vueuse/nuxt",
+    // "@nuxtjs/i18n",
+    "@nuxtjs/device",
+    "nuxt-electron",
+  ],
+});
---
+++ pages/index.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="text-blue-500">
+    home page
+  </div>
+</template>
+
+<script lang="ts" setup>
+
+</script>
+
+<style></style>
