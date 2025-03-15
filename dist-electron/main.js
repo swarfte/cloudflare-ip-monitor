dad77e11da4a5519233b58fc8a8d21194010941f
Benjamin Chau <68836494+swarfte@users.noreply.github.com>
2025-03-15 13:45:44 +0800

    update the title and icon

--- dist-electron/main.js
+++ dist-electron/main.js
@@ -1,4 +1,10 @@
-import { app as e, BrowserWindow as o } from "electron";
-e.whenReady().then(() => {
-  new o().loadURL(`${process.env.VITE_DEV_SERVER_URL}`);
+import { app as n, BrowserWindow as i } from "electron";
+import o from "path";
+import { fileURLToPath as e } from "url";
+const r = e(import.meta.url), t = o.dirname(r);
+n.whenReady().then(() => {
+  new i({
+    title: "Cloudflare Ip Monitor",
+    icon: o.join(t, "../public/icon.png")
+  }).loadURL(`${process.env.VITE_DEV_SERVER_URL}`);
 });
