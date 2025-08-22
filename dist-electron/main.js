f1e78b07cb787b76316cb6ff2fe6f9ea35e43af6
Benjamin Chau <68836494+swarfte@users.noreply.github.com>
2025-08-22 16:00:04 +0800

    Revert "Migrate project to Electron with nuxt-electron"
    This reverts commit c0fb85c419151807187260b72d28cf5586ba65c2.

--- dist-electron/main.js
+++
@@ -1,4 +0,0 @@
-import { app as e, BrowserWindow as o } from "electron";
-e.whenReady().then(() => {
-  new o().loadURL(process.env.VITE_DEV_SERVER_URL);
-});
--- electron/main.ts
+++
@@ -1,5 +0,0 @@
-import { app, BrowserWindow } from "electron";
-
-app.whenReady().then(() => {
-  new BrowserWindow().loadURL(process.env.VITE_DEV_SERVER_URL as string);
-});
